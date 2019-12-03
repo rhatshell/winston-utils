3b2872f53f4464cdd1fe37a5c7835ede98a54fa5
Robert Hamilton <Robert.Hamilton@shell.com>
2019-12-03 13:32:45 -0600

    Make runtime agnostic with support for global.window

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+// tslint:disable: no-internal-module
+declare global {
+  module NodeJS {
+    interface Global {
+      window: any;
+    }
+  }
+}
+
+if (typeof window === 'undefined') {
+  global.window = {};
+}
+
 export * from './exportDebugHooks';
 export * from './formatters';
 export * from './interfaces';
